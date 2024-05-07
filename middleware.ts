e08a6456bf61247c17a6988ce4cbabeec75c8ae3
Samuel-ma <mabilmakur@gmail.com>
2024-05-07 10:42:14 +0300

    first commit

--- middleware.ts
+++ middleware.ts
@@ -3,5 +3,5 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/"],
+    matcher: ["/((?!.*\\..*|_next).*)", "/"],
 };
